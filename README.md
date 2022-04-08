9da2a8a9f276695f5a3a1b71e7fa5bb76ca16b93
Hayden Kiltoff <hayden060@gmail.com>
2022-04-07 22:41:14 -0700

    ReadMe modified for content

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # marvelgame
+
+
+## Description
+
+This is a wonderful game to test your Marvel knowledge and see your score at the very end
+for how well you did!
+
+- My motivation for this is my love of the Marvel Universe and all the characters and stories behind it.
+- I built this to see and collect information on how well others know their Marvel information, and allow others to test themselves
+
+
+## Installation
+
+Begin by going to the homepage where you are able to begin the timed game.
+
+https://hayden1773.github.io/marvelgame/
+
+## Usage
+-Start  by clicking the "Test your Knowledge button located on the homepage
+-This will activate your timer to complete the whole quiz
+-Click through the questions with your best guess as to what the right answer is. 
+-Whether you get one correct or incorrect, it will display before moving to the next question.
+-When you complete all the questions you get to see how you did!
+-Save your initals and click submit
+-In the top left corner click the home page to try again!
+
+    ```md
+    ![Home Page](assets/Homepage.png)
+    ```
+     ```md
+    ![In-Game](assets/In-game.png)
+    ```
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -5,48 +5,48 @@ var questions = [
         correctAnswer: "The power stone"
     },
     {
-        question:"a cria is a baby what?",
-        answers:['llama',"hedgehog","pangolin","iguana"],
-        correctAnswer: "llama"
+        question:"Who is ironman?",
+        answers:["Peter Parker","Tony","Everett","Venom"],
+        correctAnswer: "Tony"
     },
     {
-        question:"About how many bears exist in the world?",
-        answers:["1 million","500 thousand", "20 million", "100 thousand"],
-        correctAnswer: "1 million"
+        question:"Who owns Marvel Studios?",
+        answers:["Elon Musk","Gerber", "Sony", "Disney"],
+        correctAnswer: "Disney"
     },
     {
-        question:"if canine means dog-like, and feline means cat-like, what does aqualine mean?",
-        answers:["manatee-like","eagle-like", "donkey-like", "fish-like"],
-        correctAnswer: "eagle-like"
+        question:"Who plays Thor?",
+        answers:["Chris Hemsworth","Donkey-kong", "Lucy Liu", "Brie Larson"],
+        correctAnswer: "Chris Hemsworth"
     },
     {
-        question:"which of these words means crab-like?",
-        answers:["caprine","cancerine", "bubaline", "murine"],
-        correctAnswer: "cancerine"
+        question:"What is Dr.Stranges building called?",
+        answers:["Hotel Motel","The laboritory", "Goofy-goobers", "The sanctum"],
+        correctAnswer: "Sanctum"
     },
     {
-        question:"What is the largest species of penguin?",
-        answers:["emperor","fairy", "chinstrap", "king"],
-        correctAnswer: "emperor"
+        question:"How are you doing so far?",
+        answers:["good","better", "best", "!amazing!"],
+        correctAnswer: "!amazing!"
     },
     {
-        question:"What do you call a group of parrots?",
-        answers:["a parliment","a pandemonium", "a prickle", "a pride"],
-        correctAnswer: "a pandemonium"
+        question:"Who hunted all the infinity stones?",
+        answers:["Moon Knight","Hulk", "Bruce Banner", "Thanos"],
+        correctAnswer: "Thanos"
     },
     {
-        question:"What do you call a group of jellyfish?",
-        answers:["a smack","a slap", "a crash", "a splash"],
-        correctAnswer: "a smack"
+        question:"Who did Hela the Goddess of death fall in-love with in the comics?",
+        answers:["Green Goblin","Thanos", "Deadpool", " Captian America"],
+        correctAnswer: "Thanos"
     },
     {
-        question:"What do you call a group of unicorns?",
-        answers:["a blessing","a miracle", "a surprise", "a boon"],
-        correctAnswer: "a blessing"
+        question:"Are you up to date on all of your Marvel?",
+        answers:["Yes","I will be", "Almost", "What is marvel?"],
+        correctAnswer: "Yes"
     },
     {
-        question:"What do you call a group of ravens?",
-        answers:["an unkindness","a congress", "a business", "an organization"],
-        correctAnswer: "an unkindness"
+        question:"Who are you most excited to see in Dr. Strange Multiverse of Madness?",
+        answers:["Dr. Strange","Variants", "X-MEN", "Marvel Zombies"],
+        correctAnswer: "Variants"
     },
 ]
